c236215c4eccbb0605782a7c72ed27c70a848a6f
matveyprupas <matveyprupas@gmail.com>
2021-01-31 19:00:08 +0200

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  let res = 0;
+  if (array == undefined || array.length == 0) return 0;
+  array.sort(function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  });
+  res = array[0];
+  return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let res = 0;
+  if (array == undefined || array.length == 0) return 0;
+  array.sort(function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }).reverse();
+  res = array[0];
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let res = 0;
+  if (array == undefined || array.length == 0) return 0;
+  for (let i = 0; i < array.length; i++) {
+    res += array[i];
+  }
+  res = res / array.length;
+  return res;
 }
